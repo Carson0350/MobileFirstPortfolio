d718680d27a538f722c64b90837835a7d83a3d3a
Carson0350 <carson.workflow@gmail.com>
2021-02-09 11:10:17 -0700

    nav bar animated

---
+++ assets/js/mian.js
@@ -0,0 +1,43 @@
+// MENU SHOW WHEN HIDDEN
+const navMenu = document.getElementById('nav-menu')
+const toggleMenu = document.getElementById('nav-toggle')
+const closeMenu = document.getElementById('nav-close')
+// SHOW
+
+toggleMenu.addEventListener('click', () => {navMenu.classList.toggle('show')})
+
+// HIDDEN
+closeMenu.addEventListener('click', () => {navMenu.classList.remove('show')})
+
+// REMOVE MENU
+const navLink = document.querySelectorAll('nav__link')
+
+const linkAction = () => {
+  navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+// SCROLL SELECTION
+
+const sections = document.querySelectorAll('section[id]')
+
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight
+    const sectionTop = current.offsetTop - 50
+    sectionId = current.getAttribute('id')
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.add('active')
+    } else {
+      document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.remove('active')
+    }
+  })
+}
+
+window.addEventListener('scroll', scrollActive)
